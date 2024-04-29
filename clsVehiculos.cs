c324a46d5396de66860cfb458b9750d4ae41276e
Francisco Elias <franeliasbv@gmail.com>
2024-04-28 22:10:16 -0300

    Agregar archivos de proyecto.

---
+++ clsVehiculos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryEliasEtapa2
+{
+    internal class clsVehiculos
+    {
+        //Características
+
+        string Nombre;
+        string Tipo;
+
+
+        public void CrearVehiculo()
+        {
+
+        }
+    }
+}
---
+++ frmVehiculos.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace pryEliasEtapa2
+{
+    partial class frmVehiculos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.btnBarco = new System.Windows.Forms.Button();
+            this.btnAvion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAuto.Location = new System.Drawing.Point(10, 291);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(120, 50);
+            this.btnAuto.TabIndex = 0;
+            this.btnAuto.Text = "Auto";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            // 
+            // btnBarco
+            // 
+            this.btnBarco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBarco.Location = new System.Drawing.Point(172, 291);
+            this.btnBarco.Name = "btnBarco";
+            this.btnBarco.Size = new System.Drawing.Size(120, 50);
+            this.btnBarco.TabIndex = 1;
+            this.btnBarco.Text = "Barco";
+            this.btnBarco.UseVisualStyleBackColor = true;
+            // 
+            // btnAvion
+            // 
+            this.btnAvion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAvion.Location = new System.Drawing.Point(342, 291);
+            this.btnAvion.Name = "btnAvion";
+            this.btnAvion.Size = new System.Drawing.Size(120, 50);
+            this.btnAvion.TabIndex = 2;
+            this.btnAvion.Text = "Avión";
+            this.btnAvion.UseVisualStyleBackColor = true;
+            // 
+            // frmVehiculos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(483, 353);
+            this.Controls.Add(this.btnAvion);
+            this.Controls.Add(this.btnBarco);
+            this.Controls.Add(this.btnAuto);
+            this.Name = "frmVehiculos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vehículos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.Button btnBarco;
+        private System.Windows.Forms.Button btnAvion;
+    }
+}
+
